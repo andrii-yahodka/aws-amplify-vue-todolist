f3d2e217d0f00d6b6296fa0d456c4685baf47902
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-11-15 21:01:46 +0200

    Add Create template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import Todos from './components/Todos.vue'
+  import Todos from './components/Todos.vue'
+  import TodosCreate from './components/Todos/Create.vue'
 </script>
 
 <template>
   <main>
-    <Todos />
+    <TodosCreate />
   </main>
 </template>
 
---
+++ src/components/Todos/Create.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+
+  import { generateClient } from "aws-amplify/data";
+  import type { Schema } from "../../../amplify/data/resource"
+
+  const client = generateClient<Schema>()
+
+  const name    = ref('')
+  const content = ref('')
+  const todos   = ref<Array<Schema['Todo']["type"]>>([])
+
+  function fetchTodos() {
+    client.models.Todo.observeQuery().subscribe(
+      {
+        next: ({ items, isSynced }) => {
+          todos.value = items
+        }
+      }
+    )
+  }
+
+  function createTodo() {
+    client.models.Todo.create(
+      {
+        name:    name.value,
+        content: content.value
+      }
+    )
+  }
+
+  onMounted(() => { fetchTodos() })
+</script>
+
+<template>
+  <input type="text" v-model="name" placeholder="Todo name">
+  <input type="text" v-model="content" placeholder="Todo description">
+  
+  <button @click="createTodo">Create Todo</button>
+
+  <h1>My todos</h1>
+  <ul>
+    <li
+      v-for="todo in todos" :key="todo.id">
+      {{ todo.name }} - {{ todo.content }}
+    </li>
+  </ul>
+</template>
