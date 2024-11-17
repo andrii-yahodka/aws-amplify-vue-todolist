c675791904114d5150536c32c0205f75536b3c86
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-11-17 20:01:51 +0200

    Add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import "./assets/main.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import "./assets/main.css"
+import { createApp } from "vue"
+import App from "./App.vue"
+import { Amplify } from "aws-amplify"
+import outputs from "../amplify_outputs.json"
 
-Amplify.configure(outputs);
+import BootstrapVue3 from 'bootstrap-vue-3'
 
-createApp(App).mount("#app");
+// Import Bootstrap CSS and BootstrapVue styles
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+Amplify.configure(outputs)
+
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
