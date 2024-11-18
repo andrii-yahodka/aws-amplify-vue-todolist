434a70525946973a0d19634d261380a511a4518b
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-11-18 21:04:29 +0200

    Add authenticator

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+  import { Authenticator } from "@aws-amplify/ui-vue"
+
   import Todos from './components/Todos.vue'
 </script>
 
 <template>
-  <main>
-    <Todos />
-  </main>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <main>
+        <Todos />
+      </main>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
 </template>
 
