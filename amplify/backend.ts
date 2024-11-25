4d3beab38057b9ed15ad8c0d72b40e97a767dab4
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-11-25 17:54:45 +0200

    Add lambda function on upload to S3 bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { storage } from './storage/resource';
 
 import { sayHello } from './functions/say-hello/resource';
 
 defineBackend({
   auth,
   data,
+  storage,
   sayHello
 });
